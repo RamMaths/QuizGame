52f35e1e2629fa551da7b959d253ce4eca32c984
RamMaths <ram.hdzven@gmail.com>
2023-05-16 09:56:13 +0900

    menu styled

--- src/Game/Mode/ModeMenu.jsx
+++ src/Game/Mode/ModeMenu.jsx
@@ -6,13 +6,13 @@ const url = 'https://opentdb.com/api.php?amount=1&difficulty=easy';
 const modes = [1, 2];
 const categories = { 
   'General Knowledge': 9,
-  'Entertainment: Books': 10,
-  'Entertainment: Film': 11,
-  'Entertainment: Music': 12,
-  'Entertainment: Musicals & Theatres': 13,
-  'Entertainment: Television': 14,
-  'Entertainment: Video Games': 15,
-  'Entertainment: Board Games': 16,
+  'Books': 10,
+  'Film': 11,
+  'Music': 12,
+  'Musicals & Theatres': 13,
+  'Television': 14,
+  'Video Games': 15,
+  'Board Games': 16,
   'Science & Nature': 17,
   'Science: Computers': 18,
   'Science: Mathematics': 19,
@@ -27,8 +27,8 @@ const categories = {
   'Vehicles': 28,
   'Entertainment: Comics': 29,
   'Science: Gadgets': 30,
-  'Entertainment: Japanese Anime & Manga': 31,
-  'Entertainment: Cartoon & Animations': 32
+  'Japanese Anime & Manga': 31,
+  'Cartoon & Animations': 32
 };
 
 
@@ -61,27 +61,33 @@ const ModeMenu = ({setSelected, getQuestion}) => {
   }
 
   return(<div className="flex flex-col justify-center items-center bg-stone-200 rounded-md drop-shadow-md text-stone-600 w-80 text-2xl" >
-    <h1 className="p-4 text-xl border-b-2 h-1/3 border-b-gray-400">Select Mode</h1>
-    <form className="flex flex-col items-start " onSubmit={start}>
+    <h1 className="pt-4 text-3xl border-b-2 h-1/3 border-b-gray-400">Select Mode</h1>
+    <form onSubmit={start}>
+      <div className="flex flex-col align-center justify-start pt-3">
+        <label htmlFor='framework'>
+          Players
+        </label>
+        <select className="bg-stone-400 mt-1 mb-3" id="mode" name="mode" value={mode} onChange={handleMode}>
+          {modes.map((mode) => {
+            return <option key={mode}>{mode}</option>;
+          })}
+        </select>
 
-      <label htmlFor='framework'>
-        Select Players
-      </label>
-      <select className="bg-stone-400 mt-1 mb-3" id="mode" name="mode" value={mode} onChange={handleMode}>
-        {modes.map((mode) => {
-          return <option key={mode}>{mode}</option>;
-        })}
-      </select>
+        <label htmlFor='framework'>
+          Category
+        </label>
+        <select className="bg-stone-400 mt-1 mb-3 w-52" id="mode" name="category" value={categ} onChange={handleCateg}>
+          {Object.keys(categories).map((category) => {
+            return <option key={category}>{category}</option>;
+          })}
+        </select>
+      </div>
 
-      <select className="bg-stone-400 mt-1 mb-3" id="mode" name="category" value={categ} onChange={handleCateg}>
-        {Object.keys(categories).map((category) => {
-          return <option key={category}>{category}</option>;
-        })}
-      </select>
-
-      { 
-        <button type='submit' className="last:items-center bg-red-500 w-20 rounded-md m-2 hover:bg-red-300 text-red-200">Start</button>
-      }
+      <div className="flex flex-col items-center">
+        { 
+          <button type='submit' className="last:items-center bg-red-500 w-20 rounded-md m-2 hover:bg-red-300 text-red-200">Start</button>
+        }
+      </div>
     </form>
   </div>);
 };
